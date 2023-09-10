86967bcb045d1daf00dcbd226a237ccb6d6b47a6
andrewDev08 <143661891+andrewDev08@users.noreply.github.com>
2023-09-10 09:54:30 +0000

    Fixing my mistakes

--- main.cs
+++ main.cs
@@ -3,18 +3,11 @@ using System;
 class Program {
   public static void Main (string[] args) {
     double conversionRate = 2.54;
+    int inchValue = 5;
     
-    Console.WriteLine("Please enter the number of inches: ");
-    var x = Console.ReadLine();
-
-    int inchValue;
-    if (int.TryParse(x, out inchValue))
-    {
-      double centiValue = inchValue * conversionRate;
-      Console.WriteLine("{0} Centimeters", centiValue );
-    }
-    else {
-      Console.WriteLine("Please only put the number" );
-    }
+    double centiValue = inchValue * conversionRate;
+    
+    Console.WriteLine("{0} Inches", inchValue );
+    Console.WriteLine("{0} Centimeters", centiValue );
   }
 }
